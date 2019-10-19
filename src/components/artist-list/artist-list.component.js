223ddcd986be1f2c7675963d8e7b6e0c2bf4d076
Johnpeace <princeolatjohnjoe@yahoo.com>
2019-10-19 17:15:21 +0100

    add some modification to artist list and artist components

--- src/components/artist-list/artist-list.component.js
+++ src/components/artist-list/artist-list.component.js
@@ -6,6 +6,7 @@ import './artist-list.styles.css';
 
 const ArtistList = () => {
   const [artists, setArtists] = useState([]);
+  const [artistAlbum, setArtistAlbum] = useState([]);
 
   useEffect(() => {
     const fetchFunc = async () => {
@@ -13,14 +14,19 @@ const ArtistList = () => {
       setArtists(response.data);
     }
     fetchFunc();
-  }, []);
-  
+  }, [setArtists]);
+
+  const handleDisplayAlbum = async e => {
+    const response = await axios('http://jsonplaceholder.typicode.com/albums'); 
+    console.log('******', response.data)
+    return setArtistAlbum(response.data);
+  }
 
   return (
     <div className='artist-list'>
       <h1>LIST OF ALL ARTISTS IN CHOCOLATE CITY</h1>
       {
-        <Artist artists={artists} />
+        <Artist artists={artists} onHandleDisplayAlbum={handleDisplayAlbum}/>
       }
     </div>
   )
--- src/components/artist/artist.component.js
+++ src/components/artist/artist.component.js
@@ -3,9 +3,16 @@ import ReactTable from "react-table";
 import './artist.styles.css';
 import "react-table/react-table.css";
 
-const Artist = ({ artists }) => {
+const Artist = ({ artists, onHandleDisplayAlbum }) => {
   const columns = [
     {
+      Header: 'User ID',
+      accessor: 'id',
+      width: 100,
+      maxWidth: 100,
+      minWidth: 100
+    },
+    {      
       Header: 'Name',
       accessor: 'name'
     },
@@ -25,13 +32,28 @@ const Artist = ({ artists }) => {
     {
       Header: 'Phone',
       accessor: 'phone'
+    },
+    {
+      Header: 'Actions',
+      Cell: props => (
+        <button
+        onClick={() => {
+          onHandleDisplayAlbum(props.original.id)
+        }}
+        >Album</button>
+      ),
+      width: 100,
+      maxWidth: 100,
+      minWidth: 100
     }
 ]
 
   return (<ReactTable
     data={artists}
     columns={columns}
-  />)
+    showPagination={false}
+    noDataText={'Please wait...'}
+    />)
 }
 
 export default Artist;
